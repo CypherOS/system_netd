db74b3437ef769261480eb106fdfef1b533cb4aa
Chris <chris@cypheros.co>
2017-04-06 13:47:38 -0400

    Merge tag 'android-7.1.2_r5' into nougat-mr1
    Android 7.1.2 Release 5
    
    Change-Id: Ie34752bfa001df8a42098710110a745c03685154

--- server/Android.mk
+++ server/Android.mk
@@ -78,6 +78,7 @@ LOCAL_SRC_FILES := \
         DnsProxyListener.cpp \
         DummyNetwork.cpp \
         DumpWriter.cpp \
+        EventReporter.cpp \
         FirewallController.cpp \
         FwmarkServer.cpp \
         IdletimerController.cpp \
@@ -104,6 +105,7 @@ LOCAL_SRC_FILES := \
         VirtualNetwork.cpp \
         main.cpp \
         oem_iptables_hook.cpp \
+		binder/android/net/metrics/INetdEventListener.aidl \
         binder/android/net/metrics/IDnsEventListener.aidl \
         QtiDataController.cpp \
 
